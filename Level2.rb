84abfc8c7f615c2981d76e81f26c3876b6db0647
megacartridge <mdltrack@gmail.com>
2016-01-03 03:29:01 -0500

    Answered Level 2

--- Level2.rb
+++ Level2.rb
@@ -1,14 +1,9 @@
-array = [ ‘1’, ‘c’, ‘0’, ‘-1’, ‘5’, ‘b’, ‘100’, ‘37’, ‘a’, ‘20’, ‘18’, ‘12’, ‘0’ ]
-#puts array
-puts "If we add 1 to each of the above numbers, and dump the rest, we get......"
-=begin
-for i in array
-    i.is_a? Integer
-    if true
-        puts "Yes"
-    else
-        puts "No"
-end
-=end
-end
+#array1 = [ ‘1’, ‘c’, ‘0’, ‘-1’, ‘5’, ‘b’, ‘100’, ‘37’, ‘a’, ‘20’, ‘18’, ‘12’, ‘0’ ]
 
+array2 = [ '1', 'c', '0', '-1', '5', 'b', '100', '37', 'a', '20', '18', '12', '0' ]
+puts array2
+puts "If we add 1 to each of the above numbers, and dump the rest, we get......"
+
+array3 = array2.select { |s| s.match(/^\d+$/) }.map(&:next)
+array3.insert(2, '0')
+puts array3
